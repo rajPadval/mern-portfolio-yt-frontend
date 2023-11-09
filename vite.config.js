dd695c2350166b5c881eae979a489fa1dfc657a1
rajPadval <rajpadval145@gmail.com>
2023-11-09 18:05:36 +0530

    Added vercel.json to client

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/admin': 'https://mern-portfolio-yt-frontend-8mut.vercel.app/admin',
-    },
-  },
 })
